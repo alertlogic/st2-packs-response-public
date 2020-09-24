edbfe5fc01b664d327093602e647a43f0706c327
Pavel Trakhtman <pavel@alertlogic.com>
2020-09-24 10:21:53 -0500

    Updated to use Instance Role

--- al_core/actions/notify_responder.py
+++ al_core/actions/notify_responder.py
@@ -1,4 +1,5 @@
 import json
+import requests
 import boto3
 
 from st2common.runners.base_action import Action
@@ -6,12 +7,12 @@ from lib.util import json_serial
 
 
 __all__ = [
-    'SQSSendMessageActionRunner'
+    'NotifyResponderActionRunner'
 ]
 
 
 # pylint: disable=too-few-public-methods
-class SQSSendMessageActionRunner(Action):
+class NotifyResponderActionRunner(Action):
     def run(self, data):
         if not isinstance(data, dict):
             return False
@@ -26,9 +27,11 @@ class SQSSendMessageActionRunner(Action):
         session_kwargs = {}
 
         # endpoint_url = self.config.get('endpoint_url', "")
-        region = self.config['aws_region']
-        session_kwargs['aws_access_key_id'] = self.config['aws_access_key_id']
-        session_kwargs['aws_secret_access_key'] = self.config['aws_secret_key']
+        region = self.config.get('aws_region', get_host_region())
+
+        if 'aws_access_key_id' in self.config:
+            session_kwargs['aws_access_key_id'] = self.config['aws_access_key_id']
+            session_kwargs['aws_secret_access_key'] = self.config['aws_secret_key']
 
         session = boto3.Session(**session_kwargs)
         client = session.client('sqs', region_name=region)
@@ -39,3 +42,10 @@ class SQSSendMessageActionRunner(Action):
         response = client.send_message(QueueUrl=queue_url, MessageBody=json.dumps(data))
         response = json.loads(json.dumps(response, default=json_serial))
         return True, response
+
+
+def get_host_region():
+    # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-identity-documents.html
+    r = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document")
+    response_json = r.json()
+    return response_json.get('region')
